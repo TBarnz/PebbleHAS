1c807550ec3f0b90daa22a19d4f4a732f15bf697
TBarnz <TBarnz@users.noreply.github.com>
2015-07-11 21:56:12 +0100

    Restful functionality added via a XMLHttpRequest
    XMLHttpRequest functionality added to POST JSON initiating a RESTFul call to add ActionItems used to update the status of a device

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 /**
- * Welcome to Pebble.js!
- *
- * This is where you write your app.
+ * HomeMenu
+ * 
  */
 var LIGHTING = 0;
 var SECURITY = 1;
@@ -20,7 +19,7 @@ var zones = [
   {
     title: "Security",
     subtitle: "Alarm/SmokeAlarm/Doors/Windows/Gates"
-  },
+      },
   {
     title: "Heating",
     subtitle: "Thermostat/Air Conditioning/Ventilation"
@@ -35,56 +34,29 @@ var zones = [
   },
 ];
   var lighting = [
-  {
-    title: "Lights",    
-  },
-  {
-    title: "Lamps",
-  },
-  {
-    title: "Strips",
-  }
+  {    title: "Lights On"   },
+  {    title: "Lights Off"    },
+  {    title: "Lights Red"    },
+  {    title: "LightsBlue"    }
 ];
 var security = [
-  {
-    title: "Alarm",    
-  },
-  {
-    title: "Doors",
-  },
-  {
-    title: "Windows",
-  }
+  {    title: "Garage Open"     },
+   {    title: "Garage Closed"     }
+ 
 ];
 var heating = [
-  {
-    title: "Air conditioning",    
-  },
-  {
-    title: "Ventilation",
-  },
-  {
-    title: "Heaters",
-  }
+  {    title: "Air conditioning"     },
+  {    title: "Ventilation"  },
+  {    title: "Heaters"  }
 ];
 var entertainment = [
-  {
-    title: "Music",    
-  },
-  {
-    title: "TV",
-  }  
+  {    title: "Music On"   },
+  {    title: "Music Off"   },
 ];
 var appliances = [
-  {
-    title: "Domestic Robot",    
-  },
-  {
-    title: "Fridge",
-  },
-  {
-    title: "Oven",
-  } 
+  {    title: "Domestic Robot",     },
+  {    title: "Fridge",  },
+  {    title: "Oven",  } 
 ];
 // Create the Menu, supplying the list of fruits
 var zoneMenu = new UI.Menu({
@@ -113,6 +85,32 @@ zoneMenu.on('select', function(e) {
       });
       // Show the Menu
       lightingMenu.show();
+      // Add a click listener for select button click
+  lightingMenu.on('select', function(e) {  
+    // Show new menu
+    switch(  e.itemIndex   ) {
+      case 0:
+         // Show the Menu
+          console.log('case 00 Lights ON!'+ e.itemIndex );
+        setLights('play');
+        break;
+      case 01:
+       console.log('case 01 Lights Off!'+ e.itemIndex );
+         setLights('stop');
+        break;
+      case 02:
+       console.log('case 01 Lights Red!'+ e.itemIndex );
+         setLights('red');
+        break;
+      case 03:
+       console.log('case 01 Lights white!'+ e.itemIndex );
+         setLights('white');
+        break;
+      default:
+        console.log('Key not recognized!'+ e.itemIndex );
+        break;
+      }
+  });
       break;
     case SECURITY:
       var securityMenu = new UI.Menu({
@@ -150,7 +148,7 @@ zoneMenu.on('select', function(e) {
        case APPLIANCES:
       var appliancestMenu = new UI.Menu({
         sections: [{
-          title: 'Aopliances options',
+          title: 'Appliance options',
           items: appliances,
           icon: 'appliances_icon.png'
         }]
@@ -162,5 +160,55 @@ zoneMenu.on('select', function(e) {
       console.log('Key not recognized!'+ e.itemIndex );
       break;
     }
+  
 });
+
+var xhrRequest = function (url, type, data, callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.onload = function () {
+    callback(this.responseText);
+  };
+  xhr.open(type, url);
+  xhr.send(data);
+};
+
+function setLights(state) {
+  // Construct URL
+     console.log('TEsting POST.... ');
+   var actionitem = {				
+				uname : "UsrKey",
+				smartitemid: 1,
+				smartitemname : "Music Player",
+				requiredstate :	state,						
+				actionstatus: "P",			
+		};
+		var jsonsa = JSON.stringify(actionitem);
+    //var url = 'http://localhost:8888/rest/has/actions/add';
+   var url = 'http://WEBSITEURL/rest/has/actions/add';
+    console.log('Url request is ' + url);
+    console.log('json is ' + jsonsa);
+ // var url = 'http://api.openweathermap.org/data/2.5/weather?lat=' +
+  //    pos.coords.latitude + '&lon=' + pos.coords.longitude;
+
+  // Send request to OpenWeatherMap
+  xhrRequest(url, 'POST', jsonsa, 
+    function(responseText) {
+      // responseText contains a JSON object with weather info
+      //return json shortly
+      //var json = JSON.parse(responseText);
+
+      // Temperature in Kelvin requires adjustment
+      
+      console.log('Response back is ' + responseText);
+
+      // Conditions
+      //conditions = json.weather[0].main;      
+      //console.log('TB Conditions are ' + conditions);
+      // Assemble dictionary using our keys          
+      
+    }      
+  );
+ }
+
+
 
